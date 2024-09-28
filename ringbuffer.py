0e50785ff6d7bf3dde93ccbfebd95ba5c717438f
NoahJVV <152837418+NoahJVV@users.noreply.github.com>
2024-09-28 10:02:29 +0800

    Finished RingBuffer.py
    Only need to add more comments

--- ringbuffer.py
+++ ringbuffer.py
@@ -15,43 +15,68 @@ class RingBuffer:
         '''
         Return number of items currently in the buffer
         '''
-
-        # TO-DO: implement this
+        return len(self.buffer)                                     # Return the length of the buffer which denotes how many items are in the buffer because we use .pop() and .insert() to add and remove items
 
     def is_empty(self) -> bool:
         '''
         Is the buffer empty (size equals zero)?
         '''
-        return len(self.buffer).
-        # TO-DO: implement this
+        return len(self.buffer) == 0                                # Return bool about if buffer is empty
         
     def is_full(self) -> bool:
         '''
         Is the buffer full (size equals capacity)?
         '''
-        # TO-DO: implement this
+        return len(self.buffer) >= capacity                         # Return bool about if buffer is at full capacity
 
     def enqueue(self, x: float):
         '''
         Add item `x` to the end
         '''
-        self.buffer.insert(0, x)        # Insert the value at the start of self.buffer
+        try:
+            if size(self) >= capacity: 
+                raise RingBufferFull                                # Raise exception if buffer is at max capacity
 
-        # AUTO DEQUEUE
-        if len(self.buffer) > capacity:     # Check if self.buffer is at max capacity
-            self.buffer.pop(capacity)       # If so, pop the end to maintain the capacity
+            self.buffer.insert(0, x)                                # Insert the value at the start of self.buffer
+            self._front = x                                         # Update the value for the front of the buffer
+            self._rear = self.buffer[-1]                            # Update the value for the back of the buffer
+
+            print(f"{x} added. Current size is {size(self)}!")      # debug
+
+        except:
+            print("Ring buffer is full!")
 
     def dequeue(self) -> float:
         '''
         Return and remove item from the front
         '''
-        self.buffer.pop(len(self.buffer))   # Remove the last (oldest) value in self.buffer
+        try:
+            if is_empty(self):
+                raise RingBufferEmpty       # Raise exception if buffer is empty
+
+            old_front = self.buffer[0]      # Save the value of the item at the front of the buffer to return later
+
+            self.buffer.pop(0)              # Remove the newest (oldest) value in self.buffer
+            self._front = self.buffer[0]    # Update the value for the front of the buffer
+            self._rear = self.buffer[-1]    # Update the value for the back of the buffer
+
+            return old_front                # Return the value of the item removed
+        
+        except:
+            print("Ring buffer is empty!")
 
     def peek(self) -> float:
         '''
         Return (but do not delete) item from the front
         '''
-        # TO-DO: implement this
+        try:
+            if is_empty(self):
+                raise RingBufferEmpty       # Raise exception if buffer is empty
+                
+            return self._front              # Return the last (newest) value in self.buffer
+
+        except:
+            print("Ring buffer is empty!")
 
 
 class RingBufferFull(Exception):
